e94989ba61ad3b1aafdf7f6970d88677444734e9
Sammy Jackson <sam.jackson@live.ca>
2020-04-15 14:43:08 -0400

    fixing inline and spacing on work menu

--- src/components/Work.js
+++ src/components/Work.js
@@ -7,31 +7,34 @@ const Work = (props) => {
     const [mouse, setMouse] = useState([0,0])
 
     const mouseOver = (e) => {
-        setMouse([e.clientX - (workRef.current.width/2), e.clientY - (workRef.current.height/2)])
-        console.log(`x: ${mouse[0]}, y: ${mouse[1]}`)
+        setMouse([e.clientX - (workRef.current.clientWidth/2), e.clientY - (workRef.current.clientHeight/2)])
         setOpacity(1)
+        //console.log(`x: ${mouse[0]}, y: ${mouse[1]}`)
+        //console.log(workRef)
     }
 
     const mouseLeave = (e) => {
-        console.log("left")
+        console.log(workRef)
         setOpacity(0)
     }
 
     return (
         <div className="work" style={props.style}>
-            {/* <div style={props.style}> */}
-                <div className="workElem">
-                    <div className="tinyText" style={{paddingLeft: '1vh'}}>
-                        {props.number}
-                    </div>
-                    <img src={arrow} className="arrow" style={{paddingLeft: '3vh'}}/>
+            <div className="workElemNumber">
+                <div className="tinyText" style={{float: 'left', paddingLeft: '1vh'}}>
+                    {props.number}
                 </div>
-                <div className="workElemTitle" onMouseMove={mouseOver} onMouseLeave={mouseLeave}>
-                    {props.name}
-                </div>
-            {/* </div> */}
-            
-            <img src={props.picture} className="workImage" ref={workRef} style={{opacity: imageOpacity, left: mouse[0], top: mouse[1]}}/>
+                <img src={arrow} className="arrow" style={{paddingLeft: '0.5vh'}}/>
+            </div>
+            <div className="workElemTitle" onMouseMove={mouseOver} onMouseLeave={mouseLeave}>
+                {props.name}
+            </div>
+            {props.video != null
+                ? <video className='workImage' ref={workRef} autoplay='' loop={true} muted={true} style={{opacity: imageOpacity, left: mouse[0], top: mouse[1]}}>
+                    <source src={props.video} type='video/mp4'></source>
+                </video>
+                : <img src={props.picture} className="workImage" ref={workRef} style={{opacity: imageOpacity, left: mouse[0], top: mouse[1]}}/>
+            }
         </div>
     )
 }
--- src/components/WorkMenu.js
+++ src/components/WorkMenu.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Work from './Work'
 import photo from '../content/photo1.png'
+import video from '../content/ballsBallFinal.mp4'
+import video2 from '../content/daily13.mp4'
 
 const WorkMenu = () => {
     return (
-        <div>
-            <Work name='RAPTORS' picture={photo} number='01'/>
-            <Work name='RAPTORS2' picture={photo} number='02' style={{top: '40vh', left: '400vh'}}/>
-            {/* <Work name='RAPTORS2' picture={photo} number='02' style={{transform: 'translate(10vw, 30vh)'}}/> */}
+        <div className='workMenu'>
+            <div style={{display: 'inline-flex'}}>
+                {/* <Work name='RAPTORS' picture={photo} number='01'/> */}
+                <Work name='RAPTORS2' picture={photo} number='02'/>
+                <Work name='RAPTORS3' video={video} number='03'/>
+                <Work name='POOP' video={video2} number='04'/>
+                {/* <Work name='RAPTORS2' picture={photo} number='02' style={{transform: 'translate(10vw, 30vh)'}}/> */}
+            </div>
         </div>
     )
 }
