1fe6e56838f27f376a6deccda6f861f93a7e3d98
Sammy Jackson <sam.jackson@live.ca>
2020-04-17 16:38:05 -0400

    improved scroll

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideBar from './SideBar'
 import Title from './Title'
 import TrishRoque from './TrishRoque'
@@ -6,25 +6,49 @@ import InvertColors from './InvertColors'
 import WorkMenu from './WorkMenu'
 
 const Main = () => {
+    const [horizontalScroll, setHorizontalScroll] = useState(0)
+    const [menuScroll, setMenuScroll] = useState(0)
+
     const onWheel = (event) => {
         event.preventDefault()
-        let scrollDistance = (100 * event.deltaY)
-        let container = document.getElementById('container')
+        if(event.deltaY < 0){ //scrollup
+            if(menuScroll > 0){
+                setMenuScroll(menuScroll - 30)
+                console.log("menuScrollDown")
+            }
+            else {
+                if(horizontalScroll < 0){
+                    setHorizontalScroll(horizontalScroll + 30) 
+                }
+            }
+        }
+        else{
+            if(horizontalScroll > -360){
+                setHorizontalScroll(horizontalScroll - 30)
+            }
+            else{
+                setMenuScroll(menuScroll + 30)
+                console.log("menuScrollUp")
+            }
+        }
+        //console.log(scrollDistance)
+        // let container = document.getElementById('container')
 
-        container.style.scrollBehavior = 'smooth'
-        container.scrollLeft += scrollDistance
+        // container.style.scrollBehavior = 'smooth'
+        // container.scrollLeft += scrollDistance
     }
 
     return (
         <div>
             <SideBar />
-            <div className="container" id="container" onWheel={onWheel}>
-                <div style={{position: 'relative', height: '100%' }}>
+            <div className="container" onWheel={onWheel}>
+                <div style={{position: 'relative', height: '100%', left: `${horizontalScroll}vh`, transition: 'left 0.25s' }}>
                     <InvertColors />
                     <Title />
                     <TrishRoque />
-                    <WorkMenu />
+                    
                 </div>
+                <WorkMenu />
             </div>
         </div>
     )
