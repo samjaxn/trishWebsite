fa6c15b6c75cc76b1a6b3164650e2acd1661ad7a
Sammy Jackson <sam.jackson@live.ca>
2020-06-09 18:45:49 -0400

    adding about circle svg and layout for about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,10 @@ const About = (props) => {
             <div className="aboutContent">
                 <div className="aboutDesc">
                     Trish Roque is a multidisciplinary designer and artist based in Toronto. Her practice is focused on the influence of designed environment on human physiological response.
-                </div>
-                <div>
                     <img src={profilePic} className="profilePic"/>
+                </div>
+                <div className="aboutWork">
+                    testing
                 </div>
             </div>
             <div className="aboutTitle">
